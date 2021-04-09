5cffc525b2c1104c2b1b5ce510fdd67f4cf7e452
WeslleyFratini <weeslleey354@gmail.com>
2021-04-08 21:38:46 -0300

    config serverless

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require ('expres')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+module.exports = app
